8f72d2d5baee6240df6e4ec626989ac96774d4df
vijaymaddu575 <86546927+vijaymaddu575@users.noreply.github.com>
2023-09-23 14:20:52 -0700

    updated the price comparison to stay

--- streamlit_app.py
+++ streamlit_app.py
@@ -20,8 +20,8 @@ dataframe = pd.read_csv(
 )
 
 # We have a limited budget, therefore we would like to exclude
-# listings with a price above 100 pounds per night
-dataframe = dataframe[dataframe["Price"] <= 100]
+# listings with a price above 100000 Rupees per night
+dataframe = dataframe[dataframe["Price"] <= 100000]
 
 # Display as integer
 dataframe["Airbnb Listing ID"] = dataframe["Airbnb Listing ID"].astype(int)
